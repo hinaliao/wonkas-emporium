381bc3b8290effaa29a1671924cbb264c78844f9
Hina Liao <hinah.zh@gmail.com>
2021-10-13 20:30:28 -0300

    minor changes

--- src/Components/HomePage/Navbar/Navbar.js
+++ src/Components/HomePage/Navbar/Navbar.js
@@ -7,10 +7,9 @@ import {
   Badge,
   Typography,
 } from "@material-ui/core";
-import { ShoppingCart, Person, MenuIcon, } from "@material-ui/icons";
+import { ShoppingCart, Person } from "@material-ui/icons";
 
-
-import logo from "/home/desapiero/project-2/src/images/logo-navbar.png";
+import logo from "../../../images/logo-navbar.png";
 import useStyles from "./styles";
 
 function Navbar({ totalItems }) {
@@ -27,56 +26,64 @@ function Navbar({ totalItems }) {
             className={style.tags}
             color="inherit"
           >
-           <img src={logo} alt="Wonka Logo" height="25px" />
+            <img
+              className={style.navLogo}
+              src={logo}
+              alt="Wonka Logo"
+              height="25px"
+            />
           </Typography>
 
-        <div className={style.centralBox}>
-          <Typography
-            component={Link}
-            to="/"
-            className={style.title}
-            color="rgba(251, 36, 72, 1)"
-          >
-            Home
-          </Typography>
+          <div className={style.centralBox}>
+            <Typography
+              component={Link}
+              to="/"
+              className={style.title}
+              color="rgba(251, 36, 72, 1)"
+            >
+              Home
+            </Typography>
 
-          <Typography
-            component={Link}
-            to="/"
-            className={style.title}
-            color="rgba(251, 36, 72, 1)"
-          >
-            About
-          </Typography>
+            <Typography
+              component={Link}
+              to="/"
+              className={style.title}
+              color="rgba(251, 36, 72, 1)"
+            >
+              About
+            </Typography>
 
-          <Typography
-            component={Link}
-            to="/"
-            className={style.title}
-            color="rgba(251, 36, 72, 1)"
-          >
-            Shop
-          </Typography>
-        </div>
+            <Typography
+              component={Link}
+              to="/"
+              className={style.title}
+              color="rgba(251, 36, 72, 1)"
+            >
+              Shop
+            </Typography>
+          </div>
           <div className={style.grow} />
           {location.pathname === "/" && (
             <div className={style.button}>
+              <IconButton>
+                <Typography
+                  component={Link}
+                  to="/"
+                  className={style.title}
+                  variant="h5"
+                  color="rgba(251, 36, 72, 1)"
+                >
+                  Login
+                </Typography>
+                <Person />
+              </IconButton>
+
               <IconButton
                 component={Link}
                 to="/cart"
                 aria-label="Show cart items"
                 color="inherit"
               >
-               <Typography
-            component={Link}
-            to="/"
-            className={style.title}
-            variant="h5"
-            color="rgba(251, 36, 72, 1)"
-          >
-            Login
-          </Typography>
-                  <Person />
                 <Badge badgeContent={totalItems} color="secondary">
                   <ShoppingCart />
                 </Badge>
