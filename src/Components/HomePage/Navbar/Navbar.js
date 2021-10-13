0dd81f5648548b0203329407528c0ebdc378d5ed
Piero de Sá <desa.piero@gmail.com>
2021-10-12 21:13:43 -0300

    working on Navbar

--- src/Components/HomePage/Navbar/Navbar.js
+++ src/Components/HomePage/Navbar/Navbar.js
@@ -1,95 +1,32 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
+import React from 'react'
+import {AppBar, Toolbar, IconButton, Badge, MetnuItem, Menu, Typography} from '@material-ui/core';
+import { CallMissedSharp, ShoppingCart } from '@material-ui/icons';
 
-export default function MenuAppBar() {
-  const [auth, setAuth] = React.useState(true);
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+import logo from "/home/desapiero/project-2/src/images/logo-navbar.png"
+import useStyles from './styles';
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAuth(event.target.checked);
-  };
-
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+const Navbar = () => {
+  const classes = useStyles();
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <FormGroup>
-        <FormControlLabel
-          control={
-            <Switch
-              checked={auth}
-              onChange={handleChange}
-              aria-label="login switch"
-            />
-          }
-          label={auth ? 'Logout' : 'Login'}
-        />
-      </FormGroup>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Photos
-          </Typography>
-          {auth && (
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-              </Menu>
-            </div>
-          )}
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    <>
+        <AppBar position="fixed" className={classes.AppBar} color="inherit"> 
+          <Toolbar>
+              <Typography variant="h6" color="inherit">
+                <img src={logo} alt="Wonka Logo" height="25px" />
+                Wonkalicious
+              </Typography>
+              <div className={classes.grow}></div>
+              <div className={classes.button}></div>
+                  <IconButton aria-label="Show cart items" color="inherit">
+                      <Badge badgeContent={2} color="secondary">
+                          <ShoppingCart />
+                      </Badge>
+                  </IconButton>
+          </Toolbar>
+        </AppBar>
+    </>
+  )
 }
+
+export default Navbar
